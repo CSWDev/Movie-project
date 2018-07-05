db7107c9016c19e4e7c16b780c722ac281276ae1
Ceirion <cei_w@hotmail.co.uk>
2018-07-05 15:48:25 +0100

    Simple movie registering site

---
+++ MVCMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
+    }
+}
